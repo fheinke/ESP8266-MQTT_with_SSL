dd5d7ac97b69abdb8e5425367e866b4c9c5d1719
Felix Heinke <43631764+fheinke@users.noreply.github.com>
2019-12-04 13:01:41 +0100

    Update ESP8266_-_MQTTSSL.ino

--- ESP8266_-_MQTTSSL/ESP8266_-_MQTTSSL.ino
+++ ESP8266_-_MQTTSSL/ESP8266_-_MQTTSSL.ino
@@ -1,14 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // 2019-12-04 Felix Heinke v0.1.0
 //
-// This Sketch is just an example, to show you how to set up a secure MQTT Connection
-// to your own MQTT Broker. This Broker is secured with an User and Password plus SSL Certificate.
-//
-// You need to change the Variables with your values.
-// To get the Fingerprint of your MQTT SSL Certificate, you can use following command
-// in a Linux shell with <MQTTBroker> as your Broker:
-// $ echo | openssl s_client -connect <MQTTBroker>:8883 | openssl x509 -fingerprint -noout
-//
 //  Device for my Test:
 //  ESP8266 D1 Mini
 //
